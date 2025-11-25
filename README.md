dc2546f2ddc058438c04032a69cd61a0e74df0bd
karina <gsanchez40320@ufide.ac.cr>
2025-11-25 16:08:52 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# proyectoprogramaci-njava
---
+++ programacionJnGrupo8/src/main/java/com/mycompany/programacionjngrupo8/ProgramacionJnGrupo8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programacionjngrupo8;
+
+/**
+ *
+ * @author karin
+ */
+public class ProgramacionJnGrupo8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
